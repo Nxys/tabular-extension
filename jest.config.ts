9fc329e1a2cf0e44a4c6dc63c4757ba78d759748
Jason <wangjian.nxys@hotmail.com>
2026-01-20 18:12:21 +0800

    .

---
+++ jest.config.ts
@@ -0,0 +1,47 @@
+/**
+ * Jest 单元测试配置
+ * 
+ * 注意：在 "type": "module" 项目中，Jest 配置必须使用 CommonJS 语法
+ */
+const config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/test/setup.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.test.json',
+      },
+    ],
+  },
+  testMatch: ['**/test/unit/**/*.test.ts'],
+  coverageDirectory: 'test/report/coverage',
+  coveragePathIgnorePatterns: ['/node_modules/', '/test/'],
+  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+    './src/background/**/*.ts': {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+    './src/content/**/*.ts': {
+      branches: 85,
+      functions: 85,
+      lines: 85,
+      statements: 85,
+    },
+  },
+};
+
+module.exports = config;
